3d57bc078b6f849c84f85e35bd7fedd9d3969a14
Morozov Artem <artemmorozov155@gmail.com>
2024-11-23 19:11:01 +0300

    В класса оценки был добавлен метод генерации таплиц с данным об оценивании + незначительные правки

--- Assessment/C_ProfileAssessment.py
+++ Assessment/C_ProfileAssessment.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
+from prettytable import PrettyTable, HRuleStyle
 from User_and_Repo.C_UserRepo import User_repo
 from Assessment.C_GPT import GPT
+import textwrap
 
 class ProfileAssessment:
     #Коэффициенты для оценки профиля
@@ -23,35 +25,37 @@ class ProfileAssessment:
     coefficient_commits = 0.2
     coefficient_count_views = 0.1
 
-    assessmen_repos = []
+    assessmen_profile_list = []
+    assessmen_repos_list = []
+    assessment_kod_list = []
+    score_profile = 0
+    average_score_repos = 0
+    score_kod = 0
     def __init__(self, user):
         self.user = user
     def assessment_profile(self):
-        assessment = (self.coefficient_followers*int(self.user.followers)
-                      + self.coefficient_following*int(self.user.following)
-                      + self.coefficient_org* len(self.user.org)
-                      + self.coefficient_languages * len(self.user.languages))
-        if self.user.hireable is not None:
-            assessment += 1 *  self.coefficient_hireable
-        if self.user.plan is not None:
-            if self.user.plan.name != "free":
-                assessment += 1 * self.coefficient_plan
-        if self.user.company is not None:
-            assessment += 1 * self.coefficient_company
-        if self.user.private_repos is not None:
-            assessment += int(self.user.private_repos)*self.coefficient_private_repos
-        if self.user.public_repos is not None:
-            assessment += int(self.user.public_repos)*self.coefficient_public_repos
-        if self.user.blog is not None and self.user.blog != "":
-            assessment += 1*self.coefficient_blog
+        self.assessmen_profile_list.append(self.coefficient_followers*int(self.user.followers))
+        self.assessmen_profile_list.append(self.coefficient_following*int(self.user.following))
+        self.assessmen_profile_list.append(1 * self.coefficient_hireable if self.user.hireable is not None else 0)
+        self.assessmen_profile_list.append(int(self.user.private_repos) * self.coefficient_private_repos if self.user.private_repos is not None else 0)
+        self.assessmen_profile_list.append(int(self.user.public_repos) * self.coefficient_public_repos if self.user.public_repos is not None else 0)
         if self.user.updated_at is not None and self.user.created_at is not None:
             years_diff = self.user.updated_at.year - self.user.created_at.year
             months_diff = self.user.updated_at.month - self.user.created_at.month
             total_months = years_diff * 12 + months_diff
             if self.user.updated_at.day < self.user.created_at.day:
                 total_months -= 1
-            assessment += total_months * self.coefficient_created_update
-        return assessment
+            self.assessmen_profile_list.append(total_months * self.coefficient_created_update)
+        else:
+            self.assessmen_profile_list.append(0)
+        self.assessmen_profile_list.append(1 * self.coefficient_plan if self.user.plan is not None and self.user.plan.name != "free" else 0)
+        self.assessmen_profile_list.append(1 * self.coefficient_blog if self.user.blog not in (None, "") else 0)
+        self.assessmen_profile_list.append(1 * self.coefficient_company if self.user.company is not None else 0)
+        self.assessmen_profile_list.append(self.coefficient_org* len(self.user.org))
+        self.assessmen_profile_list.append(self.coefficient_languages * len(self.user.languages))
+
+        self.score_profile = sum(self.assessmen_profile_list)
+        return self.score_profile
 
     def assessment_repos(self):
         assessment_repo = []
@@ -61,27 +65,82 @@ class ProfileAssessment:
             assessment_repo.append(int(self.user.repos_user[i].forks) * self.coefficient_forks)
             assessment_repo.append(int(self.user.repos_user[i].stargazers_count) * self.coefficient_stargazers_count)
             assessment_repo.append(int(self.user.repos_user[i].contributors_count) * self.coefficient_contributors_count)
+            total_days = (int((self.user.repos_user[i].last_date - self.user.repos_user[i].created_at).days))
+            assessment_repo.append(total_days * self.coefficient_created_update_r)
             assessment_repo.append(int(self.user.repos_user[i].commits) * self.coefficient_commits)
             if self.user.publicOrPrivate == "public":
                 assessment_repo.append(0)
             else:
                 assessment_repo.append(int(self.user.repos_user[i].count_views) * self.coefficient_count_views)
-            total_days = (int((self.user.repos_user[i].last_date - self.user.repos_user[i].created_at).days))
-            assessment_repo.append(total_days * self.coefficient_created_update_r)
             copy_assessment_repo = deepcopy(assessment_repo)
-            self.assessmen_repos.append(copy_assessment_repo)
+            self.assessmen_repos_list.append(copy_assessment_repo)
             for j in range (len(assessment_repo)):
                 overall_assessment += assessment_repo[j]
             assessment_repo.clear()
-        average_score = overall_assessment / len(self.user.repos_user)
-        return average_score
+        self.average_score_repos = overall_assessment / len(self.user.repos_user)
+        return self.average_score_repos
+
     def assessment_kod(self, full_or_three):
         list_of_path = User_repo.dounloud_mainRepo(self.user.main_repo)
         chat_gpt = GPT(list_of_path)
-        list_test = chat_gpt.evaluate_codeS(full_or_three)
-        kod_mark = 0
-        for i in range (len(list_test)):
-            text, marks = list_test[i]
-            kod_mark+=marks
-        kod_mark = (kod_mark/len(list_test))*5
-        return kod_mark
+        self.assessment_kod_list = chat_gpt.evaluate_codeS(full_or_three)
+        for i in range (len(self.assessment_kod_list)):
+            text, marks, file_name = self.assessment_kod_list[i]
+            self.score_kod+=marks
+        self.score_kod = (self.score_kod/len(self.assessment_kod_list))*5
+        return self.score_kod
+    def print_assessment(self):
+        tables = []
+
+        #Данные об оценке профиля
+        x = PrettyTable(hrules=HRuleStyle.ALL)
+        x.field_names = ["Field name", "Assessment"]
+        x.add_row(["Количество подписчиков", self.assessmen_profile_list[0]])
+        x.add_row(["Количество подписок", self.assessmen_profile_list[1]])
+        x.add_row(["Доступность для найма", self.assessmen_profile_list[2]])
+        x.add_row(["Количество приватных репозиториев", self.assessmen_profile_list[3]])
+        x.add_row(["Количество публичных репозиториев", self.assessmen_profile_list[4]])
+        x.add_row(["Продолжительность пользования", self.assessmen_profile_list[5]])
+        x.add_row(["Подписка", self.assessmen_profile_list[6]])
+        x.add_row(["Блог", self.assessmen_profile_list[7]])
+        x.add_row(["Компания", self.assessmen_profile_list[8]])
+        x.add_row(["Организации", self.assessmen_profile_list[9]])
+        x.add_row(["Языки программирования", self.assessmen_profile_list[10]])
+        x.align["Field name"] = "l"  # Выравнивание текста в столбце
+        x.align["Assessment"] = "r"  # Выравнивание текста в столбце
+        x.border = True  # Отображать границы таблицы
+        x.header = True  # Отображать заголовок таблицы
+        x.padding_width = 1  # Отступ между ячейками
+        tables.append(x)
+
+        #Данные об оценке репозиториев
+        for i in range (len(self.assessmen_repos_list)):
+            x_r = PrettyTable(hrules=HRuleStyle.ALL)
+            x_r.field_names = ["Field name", "Assessment"]
+            x_r.add_row(["Количество веток", self.assessmen_repos_list[i][0]])
+            x_r.add_row(["Количество звезд", self.assessmen_repos_list[i][1]])
+            x_r.add_row(["Количество контрибьютеров", self.assessmen_repos_list[i][2]])
+            x_r.add_row(["Продолжительность работы", self.assessmen_repos_list[i][3]])
+            x_r.add_row(["Количество коммитов внутри репозитория", self.assessmen_repos_list[i][4]])
+            x_r.add_row(["Количество просмотров репозитория", self.assessmen_repos_list[i][5]])
+            x_r.align["Field name"] = "l"  # Выравнивание текста в столбце
+            x_r.align["Assessment"] = "r"  # Выравнивание текста в столбце
+            x_r.border = True  # Отображать границы таблицы
+            x_r.header = True  # Отображать заголовок таблицы
+            x_r.padding_width = 1  # Отступ между ячейками
+            tables.append(x_r)
+
+        #Данные об оценке кода
+        x_y_r = PrettyTable(hrules=HRuleStyle.ALL)
+        x_y_r.field_names = ["Field name", "Assessment", "Explanation"]
+        for j in range (len(self.assessment_kod_list)):
+            text, marks, file_name = self.assessment_kod_list[j]
+            x_y_r.add_row([file_name, marks, textwrap.fill(text, width=50)])
+        x_y_r.align["Field name"] = "l"  # Выравнивание текста в столбце
+        x_y_r.align["Assessment"] = "l"  # Выравнивание текста в столбце
+        x_y_r.align["Explanation"] = "l"  # Выравнивание текста в столбце
+        x_y_r.border = True  # Отображать границы таблицы
+        x_y_r.header = True  # Отображать заголовок таблицы
+        x_y_r.padding_width = 1  # Отступ между ячейками
+        tables.append(x_y_r)
+        return tables
--- M_TakeData.py
+++ M_TakeData.py
@@ -7,28 +7,37 @@ def take_data (user, publicOrPrivate):
                            user.updated_at, user.created_at, user.plan, user.blog, user.get_repos(), user.company, user.get_orgs(), publicOrPrivate)
     assessment = ProfileAssessment(user_git)
     assessment_profile = assessment.assessment_profile()
-    assessment_repos = assessment.assessment_repos()
-    assessment_kod = 0 #Оценка кода
-    print("Желаете ли вы оценить код внутри одного репозитория? (репозиторий выбирался по количеству комитов, звезд, ветвей и простмотров)")
-    print("Оценка кода займет достаточно много времени (от 1 минуты до 15)")
-    var_kod = int(input("Введите 1, если - оценить, введите 2 - оценка не нужна: "))
-    if var_kod == 1:
-        print("Если репозиторий содержит много файлов с кодом, оценивать все или первые три?")
-        var_kod_2 = int(input("Введите 1, если - оценить все, введите 2 - оценить первые три: "))
-        assessment_kod = assessment.assessment_kod(var_kod_2)
-    if var_kod == 1:
-        print(f"Оценка профиля: {assessment_profile}, Оценка репозиториев: {assessment_repos}, Оценка кода: {assessment_kod}")
+    if publicOrPrivate == "public" and user_git.public_repos == 0:
+        assessment_repos = 0
+        assessment_kod = 0
+        print("У пользователя не обнаружены репозитории")
+        print(f"Оценка профиля: {assessment_profile}")
+    elif publicOrPrivate == "private" and user_git.public_repos+ user_git.private_repos == 0:
+        assessment_repos = 0
+        assessment_kod = 0
+        print("У пользователя не обнаружены репозитории")
+        print(f"Оценка профиля: {assessment_profile}")
     else:
-        print(f"Оценка профиля: {assessment_profile}, Оценка репозиториев: {assessment_repos}")
-    assessmet = assessment_profile + assessment_repos + assessment_kod
-    print(f"Общая оценка: {assessmet}")
+        assessment_repos = assessment.assessment_repos()
+        assessment_kod = 0
+        print(
+            "Желаете ли вы оценить код внутри одного репозитория? (репозиторий выбирался по количеству комитов, звезд, ветвей и простмотров)")
+        print("Оценка кода займет достаточно много времени (от 1 минуты до 15)")
+        var_kod = int(input("Введите 1, если - оценить, введите 2 - оценка не нужна: "))
+        if var_kod == 1:
+            print("Если репозиторий содержит много файлов с кодом, оценивать все или первые три?")
+            var_kod_2 = int(input("Введите 1, если - оценить все, введите 2 - оценить первые три: "))
+            assessment_kod = assessment.assessment_kod(var_kod_2)
+        print(f"Оценка профиля: {assessment_profile}, Оценка репозиториев: {assessment_repos}, Оценка кода: {assessment_kod}")
+        assessmet = assessment_profile + assessment_repos + assessment_kod
+        print(f"Общая оценка: {assessmet}")
     print("Желаете ли вы получить подробную информацию об оценке?\n"
           " 1 - если желаете загрузить файл с информациеей\n"
           " 2 - если выгрузить всю подробную информацию в консоль\n"
           " 3 - если вам не нужна подробная информация")
     var = int(input("Введите число от 1-го до 3-ех: "))
     if var == 1:
-        save_user_information(user_git)
+        save_user_information(user_git, assessment)
     elif var == 2:
         tables = user_git.Print_user_information()
         for i in range(len(tables)):
