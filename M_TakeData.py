78c3c025174734adea8e4e38f13d0147d6a647fd
Morozov Artem <artemmorozov155@gmail.com>
2024-11-24 18:26:36 +0300

    I allocated the generation of tables with information to a separate file

--- M_TakeData.py
+++ M_TakeData.py
@@ -1,12 +1,14 @@
 from User_and_Repo.C_User import User_GitHub
 from Assessment.C_ProfileAssessment import ProfileAssessment
 from Interface.M_SaveInformation import save_user_information
+from M_GetInformation import print_assessment
 
 def take_data (user, publicOrPrivate):
     user_git = User_GitHub(user.login, user.followers, user.following, user.hireable, user.owned_private_repos, user.public_repos,
                            user.updated_at, user.created_at, user.plan, user.blog, user.get_repos(), user.company, user.get_orgs(), publicOrPrivate)
     assessment = ProfileAssessment(user_git)
     assessment_profile = assessment.assessment_profile()
+    var_kod = 2
     if publicOrPrivate == "public" and user_git.public_repos == 0:
         assessment_repos = 0
         assessment_kod = 0
@@ -20,8 +22,7 @@ def take_data (user, publicOrPrivate):
     else:
         assessment_repos = assessment.assessment_repos()
         assessment_kod = 0
-        print(
-            "Желаете ли вы оценить код внутри одного репозитория? (репозиторий выбирался по количеству комитов, звезд, ветвей и простмотров)")
+        print("Желаете ли вы оценить код внутри одного репозитория? (репозиторий выбирался по количеству комитов, звезд, ветвей и простмотров)")
         print("Оценка кода займет достаточно много времени (от 1 минуты до 15)")
         var_kod = int(input("Введите 1, если - оценить, введите 2 - оценка не нужна: "))
         if var_kod == 1:
@@ -31,15 +32,17 @@ def take_data (user, publicOrPrivate):
         print(f"Оценка профиля: {assessment_profile}, Оценка репозиториев: {assessment_repos}, Оценка кода: {assessment_kod}")
         assessmet = assessment_profile + assessment_repos + assessment_kod
         print(f"Общая оценка: {assessmet}")
+
+
     print("Желаете ли вы получить подробную информацию об оценке?\n"
           " 1 - если желаете загрузить файл с информациеей\n"
           " 2 - если выгрузить всю подробную информацию в консоль\n"
           " 3 - если вам не нужна подробная информация")
     var = int(input("Введите число от 1-го до 3-ех: "))
     if var == 1:
-        save_user_information(user_git, assessment)
+        save_user_information(user_git, assessment, var_kod)
     elif var == 2:
-        tables = user_git.Print_user_information()
+        tables = print_assessment(user_git, assessment, var_kod)
         for i in range(len(tables)):
             print(str(tables[i]))
             if i == 0:
--- User_and_Repo/C_User.py
+++ User_and_Repo/C_User.py
@@ -1,4 +1,3 @@
-from prettytable import PrettyTable, HRuleStyle
 from Interface.C_ProgressBar import ProgressBar
 from User_and_Repo.C_UserRepo import User_repo
 
@@ -49,48 +48,3 @@ class User_GitHub:
                 self.languages.append(repo_user.language)
         prbar.closePd()
         self.main_repo = User_repo.serch_repo(self.repos, self.judgement_rName)
-
-
-    def Print_user_information(self):
-        tables = []
-        x = PrettyTable(hrules=HRuleStyle.ALL)
-        x.field_names = ["Field name", "Significance"]
-        x.add_row(["Доступ к профилю", self.publicOrPrivate])
-        x.add_row(["Имя пользователя",self.name ])
-        x.add_row(["Количество подписчиков", self.followers])
-        x.add_row(["Количество подписок", self.following])
-        x.add_row(["Доступность для найма", self.hireable])
-        x.add_row(["Количество приватных репозиториев", self.private_repos])
-        x.add_row(["Количество публичных репозиториев", self.public_repos])
-        x.add_row(["Дата создания аккаунта", self.created_at])
-        x.add_row(["Дата последнего изменения", self.updated_at])
-        x.add_row(["Подписка", self.plan])
-        x.add_row(["Ссылка на блог", self.blog])
-        x.add_row(["Компания", self.company])
-        x.add_row(["Организации", ' '.join(map(str, self.org))])
-        x.add_row(["Языки программирования", ' '.join(map(str, self.languages))])
-        x.align["Field name"] = "l"  # Выравнивание текста в столбце
-        x.align["Significance"] = "r"  # Выравнивание текста в столбце
-        x.border = True  # Отображать границы таблицы
-        x.header = True  # Отображать заголовок таблицы
-        x.padding_width = 1  # Отступ между ячейками
-        tables.append(x)
-        for i in range (len(self.repos_user)):
-            x_r = PrettyTable(hrules=HRuleStyle.ALL)
-            x_r.field_names = ["Field name", "Significance"]
-            x_r.add_row(["Название репозитория", self.repos_user[i].name])
-            x_r.add_row(["Язык программирования", self.repos_user[i].language])
-            x_r.add_row(["Количество веток", self.repos_user[i].forks])
-            x_r.add_row(["Количество звезд", self.repos_user[i].stargazers_count])
-            x_r.add_row(["Количество контрибьютеров", self.repos_user[i].contributors_count])
-            x_r.add_row(["Дата создания репозитория", self.repos_user[i].created_at])
-            x_r.add_row(["Дата последнего изменения репозитория", self.repos_user[i].last_date])
-            x_r.add_row(["Количество коммитов внутри репозитория", self.repos_user[i].commits])
-            x_r.add_row(["Количество просмотров репозитория", self.repos_user[i].count_views])
-            x_r.align["Field name"] = "l"  # Выравнивание текста в столбце
-            x_r.align["Significance"] = "r"  # Выравнивание текста в столбце
-            x_r.border = True  # Отображать границы таблицы
-            x_r.header = True  # Отображать заголовок таблицы
-            x_r.padding_width = 1  # Отступ между ячейками
-            tables.append(x_r)
-        return tables
