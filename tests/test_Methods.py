e1a02ec6c20ed356044064fc15a1a99bcfa2cc32
Morozov Artem <artemmorozov155@gmail.com>
2024-12-07 19:22:23 +0300

    Fix tests and pront for gpt

--- tests/test_Methods.py
+++ tests/test_Methods.py
@@ -3,14 +3,20 @@ import os
 import math
 from Assessment.C_ProfileAssessment import ProfileAssessment
 from User_and_Repo.C_UserRepo import User_repo
+from User_and_Repo.C_User import User_GitHub
 from StartMethods.M_Authentication import Login, LoginPassword, LoginToken
+from Assessment.C_ProfileAssessment import ProfileAssessment
 from github import Github
 from github import Auth
 
 MyToken = os.environ.get("GITHUB_TOKEN")
 
+user = Login(MyToken, "Lucik19")
+user_git = User_GitHub(user, "public")
+assessment = ProfileAssessment(user_git)
+
 @pytest.mark.parametrize("frequencyCommits, coefficient_frequencyCommits" , [(1 , 5), (0.5, 5), (2, 5), (23 ,5), (0.01, 5), (0, 5)])
-def test_frequencyCommits_to_score_exp(frequencyCommits, coefficient_frequencyCommits):
+def test_frequencyCommits_to_score_exp(frequencyCommits, coefficient_frequencyCommits ):
     decay_rate = 0.5
     if frequencyCommits != "NULL":
         actual_score =  coefficient_frequencyCommits * math.exp(-decay_rate * frequencyCommits)
@@ -19,17 +25,17 @@ def test_frequencyCommits_to_score_exp(frequencyCommits, coefficient_frequencyCo
 
 @pytest.mark.parametrize("followers" , [(0), (10), (100), (1000), (3000), (5000), (5500)])
 def test_followers_to_score_log(followers):
-    actual_score = ProfileAssessment.followers_to_score_log(ProfileAssessment, followers)
+    actual_score = assessment.followers_to_score_log(followers)
     print(f"{followers} - {actual_score}")
 
 @pytest.mark.parametrize("following" , [(0), (5), (10), (100), (150), (200), (400)])
 def test_following_to_score_log(following):
-    actual_score = ProfileAssessment.following_to_score_log(ProfileAssessment, following)
+    actual_score = assessment.following_to_score_log(following)
     print(f"{following} - {actual_score}")
 
 @pytest.mark.parametrize("language" , [(0), (1), (3), (5), (7), (10), (15)])
 def test_language_to_score_log(language):
-    actual_score = ProfileAssessment.language_to_score_log(ProfileAssessment, language)
+    actual_score = assessment.language_to_score_log(language)
     print(f"{language} - {actual_score}")
 
 @pytest.mark.parametrize("inDayCommits, day_work" , [(2, 1), (2, 2), (1.7692307692307692, 1), (1.7692307692307692, 2), (1000, 1), (1000, 2), (1000, 3)])
@@ -38,9 +44,8 @@ def test_inDayCommits(inDayCommits,day_work ):
     coefficient_inDayCommits_repo = 20
     coeffOneDay_inDayCommits_repo = 5
     coefficient_inDayCommits = (coefficient_inDayCommits_repo if day_work != 1 else coeffOneDay_inDayCommits_repo)
-    score = ProfileAssessment.inDayCommits_to_score_log(ProfileAssessment, inDayCommits, coefficient_inDayCommits, max_value_inDayCommitsRepo)
+    score = assessment.inDayCommits_to_score_log(inDayCommits, coefficient_inDayCommits, max_value_inDayCommitsRepo)
     print(f"{inDayCommits}, {day_work} - {score}")
-
 
 
 @pytest.mark.parametrize("MyToken, login" , [(MyToken, "MorozkoArt"), (MyToken, "ProNinjaDev")])
