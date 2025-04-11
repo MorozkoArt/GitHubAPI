0eea8bf1d88bca0153c09e49766a34b680350774
Morozov Artem <artemmorozov155@gmail.com>
2025-04-11 14:33:23 +0300

    creation of a model and its subsequent training without upgrades

--- src/ml/C_dataset.py
+++ src/ml/C_dataset.py
@@ -1,14 +1,27 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+import numpy as np
 
 
 class GitHubDataset(Dataset):
-    def __init__(self, features, labels):
-        self.features = torch.tensor(features.values, dtype=torch.float32)
-        self.labels = torch.tensor(labels.values, dtype=torch.float32)
+    def __init__(self, features, targets, transform=None):
+        self.features = torch.tensor(features.values if hasattr(features, 'values')
+                                     else features.astype(np.float32), dtype=torch.float32)
+        self.targets = torch.tensor(targets.values if hasattr(targets, 'values')
+                                    else targets.astype(np.float32), dtype=torch.float32)
+        self.transform = transform
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.targets)
 
     def __getitem__(self, idx):
-        return self.features[idx], self.labels[idx]
+        sample = (self.features[idx], self.targets[idx])
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+    def get_feature_dim(self):
+        return self.features.shape[1]
+
+    def get_target_dim(self):
+        return self.targets.shape[1]
